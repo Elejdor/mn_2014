38a30f418f29ac7ff0ec11bd71a930799eea74b3
Elejdor <elejdor@gmail.com>
2014-04-27 16:51:35 +0200

    Added interpolation project, newton's interpolation and constant nodes distances.

---
+++ Zad3/Interpolation/Debug.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolation
+{
+    static class Debug
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] " + message);
+        }
+    }
+}
---
+++ Zad3/Interpolation/FormPlot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Interpolation
+{
+    public partial class FormPlot : Form
+    {
+        public FormPlot(List<Point> nodes, List<Point> interpolated, Func<double, double> function)
+        {
+            InitializeComponent();
+            foreach (Point element in interpolated)
+            {
+                chart1.Series["Interpolation"].Points.AddXY(element.x, element.y);
+            }
+
+            foreach (Point element in nodes)
+            {
+                chart1.Series["Nodes"].Points.AddXY(element.x, element.y);
+            }
+
+            for (double x = nodes[0].x; x < nodes[nodes.Count-1].x; x += 0.01f)
+            {
+                chart1.Series["Function"].Points.AddXY(x, function(x));
+            }
+        }
+
+        private void FormPlot_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
